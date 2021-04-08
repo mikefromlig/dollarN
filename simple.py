f94bce71b60df3c23c51bdc377eba488c36e2c8a
mikefromlig <michael.ortega@imag.fr>
2021-04-08 07:41:05 +0200

    Simplifiying simple.py

--- simple.py
+++ simple.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # Michael ORTEGA LIG/CNRS - 02/Apr/2021
 
-import numpy as np
 from dollarN import dollarN
 
 dN = dollarN()
@@ -10,7 +9,5 @@ dN = dollarN()
 dN.add_gesture('X', [   [[0.,0.], [5.,5.]], [[0.,5.], [5.,0.]]      ])
 dN.add_gesture('T', [   [[0.,5.], [5.,5.]], [[2.5,0.], [2.5,5.]]    ])
 
-w = np.array([[0, 5.2], [5.,5.]])
-x = np.array([[2.5, 0.], [2.5,5.]])
-res = dN.recognize([w, x])
-print(res)
+test = [[[0, 5.2], [5.,5.]], [[2.5, 0.], [2.5,5.]]]
+print( dN.recognize(test) )
